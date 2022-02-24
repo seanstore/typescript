6c8da3efeb76325cc31d4f10d18e45b29d6b01bf
seanstore <seanstore.dev@gmail.com>
2022-02-24 10:15:56 +0900

    Add Intersection Type Examples

---
+++ types/1-7-intersection.ts
@@ -0,0 +1,22 @@
+{
+  // Intersection Type: &
+  type Student = {
+    name: string;
+    score: number;
+  };
+  type Worker = {
+    employeeId: number;
+    work: () => void;
+  };
+
+  function internWork(person: Student & Worker) {
+    console.log(person.name, person.employeeId, person.work);
+  }
+
+  internWork({
+    name: "Sean",
+    score: 100,
+    employeeId: 1,
+    work: () => {},
+  });
+}
